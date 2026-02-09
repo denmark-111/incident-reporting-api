ed20ca2d4eb0b3e1bec5a391d1ca59a61ea625b6
denmark-111 <maglipon.denmarc.amar@gmail.com>
2026-02-09 22:40:05 +0800

    chore(api): move ComplaintController inside Api folder

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\ComplaintController;
+use App\Http\Controllers\Api\ComplaintController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
 
-    //Complaints
-    Route::apiResource('complaints', ComplaintController::class)->only(['index', 'show', 'store', 'update']);
-
-});
+//Complaints
+Route::apiResource('complaints', ComplaintController::class)->only(['index', 'show', 'store', 'update']);
